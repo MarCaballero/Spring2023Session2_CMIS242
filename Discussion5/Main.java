8068e8e7232d32e0bc478fc89d4dd496445396bf
Melissa Caballero <caballeromelissa84@gmail.com>
2023-04-27 13:21:54 -0400

    I used Discussion 5 assignment for Discussion 7 assignment where I implemented try and catch statements to throw errors whenever user input was invalid and/or incorrect

--- Discussion5/Main.java
+++ Discussion5/Main.java
@@ -11,7 +11,7 @@
 
 import java.util.*;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //creating Scanner object
         Scanner input = new Scanner(System.in);
         System.out.println("----------Welcome to the reservation system!----------");
@@ -20,31 +20,86 @@ public class Main {
         System.out.println("2. Mexican");
         System.out.println("3. Exit");
         System.out.print("Enter your choice: ");
-        int choice = input.nextInt();
-        int numReservations = 0;
+        int choice = 0;
+        /**
+         * This try-catch block will catch any invalid input from the user.
+         * If the user enters a number that is not between 1 and 3, the program will ask the user to enter a valid input.
+         * If the user enters letters instead of numbers, the program will throw an exception and exit the program.
+         */
+        try{
+            choice = input.nextInt();
+            if(choice < 1 || choice > 3){
+                System.out.print("Sorry, you have entered an invalid input. Please try again: ");
+                choice = input.nextInt();
+            }
+        }catch(InputMismatchException e){
+            throw new InputMismatchException("Sorry, you have entered letters instead of numbers. Please try again.");
+        }
+        /**
+         * This while loop will keep running until the user enters 3 to exit the program.
+         */
         while(choice != 3){
+            //This variable will keep track of the number of reservations the user wants to make
+            int numReservations = 0;
             //If the user chose is 1 then reservation will go to the Italian Restaurant
             if(choice == 1){
-                ItalianRestaurant italian = new ItalianRestaurant();
                 System.out.print("How many tables would you like to reserve? ");
-                numReservations = input.nextInt();
-                italian.makeReservation(numReservations);
+                /**
+                 * This try-catch block will catch any invalid input from the user.
+                 * If the user enters a negative number, the program will ask the user to enter a valid input.
+                 * If the user enters letters instead of numbers, the program will throw an exception and exit the program.
+                 */
+                try{
+                    numReservations = input.nextInt();
+                    if(numReservations < 0){
+                        System.out.print("Sorry, you have entered a negative number. Please try again: ");
+                        numReservations = input.nextInt();
+                    }
+                }catch(Exception e){
+                    throw new Exception("Sorry, you have entered letters instead of numbers. Please try again: ");
+                }
+                //Creating an object of the ItalianRestaurant class
+                ItalianRestaurant italian = new ItalianRestaurant();
+                italian.makeReservation(numReservations);   
                 italian.displayReservations(numReservations);
                 System.out.println();
-            }
+                }
+
             //If the user chose is 2 then reservation will go to the Mexican Restaurant
             else if(choice == 2){
+                System.out.print("How many tables would you like to reserve? ");
+                /**
+                 * This try-catch block will catch any invalid input from the user.
+                 * If the user enters a negative number, the program will ask the user to enter a valid input.
+                 * If the user enters letters instead of numbers, the program will throw an exception and exit the program.
+                 */
+                try{
+                    numReservations = input.nextInt();
+                    if(numReservations < 0){
+                        System.out.print("Sorry, you have entered a negative number. Please try again: ");
+                        numReservations = input.nextInt();
+                    }
+                }catch(Exception e){
+                    throw new Exception("Sorry, you have entered letters instead of numbers. Please try again: ");
+                }
+                //Creating an object of the MexicanRestaurant class
                 MexicanRestaurant mexican = new MexicanRestaurant();
-                System.out.println("How many tables would you like to reserve? ");
-                numReservations = input.nextInt();
                 mexican.makeReservation(numReservations);
                 mexican.displayReservations(numReservations);
                 System.out.println();
             }
-
             //At the end the user is ask if he wants to see the reservation menu again
             System.out.println("Would you like to see the menu again (true, false)? ");
-            boolean menu = input.nextBoolean();
+            boolean menu;
+            /**
+             * This try-catch block will catch any invalid input from the user.
+             * If the user enters any other input than true or false, the program will throw an exception and exit the program.
+             */
+            try{
+                menu = input.nextBoolean();
+            }catch(InputMismatchException e){
+                throw new InputMismatchException("Invalid input. Please enter true or false.");
+            }
             //if is false then the program will end
             if(menu == false){
                 break;
@@ -56,14 +111,11 @@ public class Main {
             System.out.println("2. Mexican");
             System.out.println("3. Exit");
             System.out.print("Enter your choice: ");
-            choice = input.nextInt();
+            choice = input.nextInt();    
         }
         //Goodbye message
         System.out.println();
         System.out.println("Thank you for using the reservation system. See you next time!");
         input.close();
-    
-
     }
-    
 }
